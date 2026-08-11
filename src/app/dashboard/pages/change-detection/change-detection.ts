2cf246754debbfd630dfd10dbce7129865aa569e
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-11 11:43:21 -0400

    Llenada vista ChangeDetection

--- src/app/dashboard/pages/change-detection/change-detection.ts
+++ src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,14 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { Title } from '@shared/title/title';
+import { ChangeDetectionStrategy, Component, signal, computed } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-change-detection',
-  imports: [],
+  imports: [Title, JsonPipe],
   templateUrl: './change-detection.html',
   styles: `
     :host {
       display: block;
     }
   `,
-  changeDetection: ChangeDetectionStrategy.Eager,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ChangeDetection {}
+export default class ChangeDetection {
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  public currentFramework = computed(() => `Change detection - ${this.frameworkAsSignal().name}`)
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update((value) => ({
+        ...value,
+        name: 'React',
+      }));
+      console.log('Hecho');
+    }, 3000);
+  }
+}
